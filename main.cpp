4e5ed6bedbb762cb935403b7b17dceb384631ec4
Daniel <wdstott@hotmail.co.uk>
2018-07-22 16:06:46 +0100

    Added Comments. Changed variable name to match requirements.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cassert>
 
-static const std::string base64_chars =
+static const std::string base64_table =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 std::string base64_encode(const std::string &input)
@@ -14,8 +14,10 @@ std::string base64_encode(const std::string &input)
 
     for(unsigned int i = 0; i < arraySize;)
     {
+        //Takes 3 characters from the character array to be encoded
         for(unsigned int j = 0; j < 3; j++)
         {
+            //Checks if there's still characters left to be encoded
             if(i <= arraySize)
             {
                 standardChar[j] = charArray[i++];
@@ -24,27 +26,35 @@ std::string base64_encode(const std::string &input)
             i++;
         }
 
+        //If there is less than 3 characters left to be encoded
         if(i > arraySize)
         {
             unsigned int charsLeft = (i - arraySize);
             if(charsLeft <= 2)
             {
-                output += base64_chars[standardChar[0] >> 2];
-                output += base64_chars[((standardChar[0] & 0x03) << 4) + ((standardChar[1] & 0xf0) >> 4)];
+                output += base64_table[standardChar[0] >> 2];
+                output += base64_table[((standardChar[0] & 0x03) << 4) + ((standardChar[1] & 0xf0) >> 4)];
             }
             if(charsLeft == 1)
             {
-                output += base64_chars[((standardChar[1] & 0xf) << 2) + ((standardChar[2] & 0xc0) >> 6)];
+                output += base64_table[((standardChar[1] & 0xf) << 2) + ((standardChar[2] & 0xc0) >> 6)];
             }
             break;
         }
 
-        output += base64_chars[standardChar[0] >> 2];
-        output += base64_chars[((standardChar[0] & 0x03) << 4) + ((standardChar[1] & 0xf0) >> 4)];
-        output += base64_chars[((standardChar[1] & 0xf) << 2) + ((standardChar[2] & 0xc0) >> 6)];
-        output += base64_chars[standardChar[2] & 0x3f];
+        //encodes the characters into base64
+
+        //Takes the first 6 bits from the first character
+        output += base64_table[standardChar[0] >> 2];
+        //Takes the last 2 bits of the first character and first 4 of the second character
+        output += base64_table[((standardChar[0] & 0x03) << 4) + ((standardChar[1] & 0xf0) >> 4)];
+        //Takes the last 4 six bits of the second character and the first 2 of the third character
+        output += base64_table[((standardChar[1] & 0xf) << 2) + ((standardChar[2] & 0xc0) >> 6)];
+        //Takes the last 6 bits from the last character
+        output += base64_table[standardChar[2] & 0x3f];
     }
 
+    //Adds padding
     if(paddingSize == 16)
     {
         output += "=";
